a010cda56e92eeb0e9d196ec53a9546918e89f06
ken3d06 <kennethfukizi@ymail.com>
2018-11-16 20:48:28 +0200

    Thread semaphore and queue helper class

---
+++ Main/ThreadSemaphore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace Main
+{
+    /// <summary>
+    /// Track the semaphore and context queue associated with a worker thread.
+    /// </summary>
+    class ThreadSemaphore
+    {
+        protected Semaphore semaphore;
+        protected ConcurrentQueue<HttpListenerContext> requests;
+        public int QueueCount => requests.Count;
+
+        public ThreadSemaphore()
+        {
+            semaphore = new Semaphore(0, Int32.MaxValue);
+            requests = new ConcurrentQueue<HttpListenerContext>();
+        }
+        /// <summary>
+        /// Enqueue request context and release semaphore that a thread is waiting on
+        /// </summary>
+        /// <param name="context"></param>
+        public void Enqueue(HttpListenerContext context)
+        {
+            requests.Enqueue(context);
+            semaphore.Release();
+        }
+        /// <summary>
+        /// Wait for the semaphore to be released
+        /// </summary>
+        public void WaitOne()
+        {
+            semaphore.WaitOne();
+        }
+
+        public bool TryDequeue(out HttpListenerContext context)
+        {
+            return requests.TryDequeue(out context);
+        }
+    }
+}
